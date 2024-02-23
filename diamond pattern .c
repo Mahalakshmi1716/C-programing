b876f582e19ce44ca1a49c7b701ef15b7b3f350b
Mahalakshmi1716 <161062244+Mahalakshmi1716@users.noreply.github.com>
2024-02-23 22:32:19 +0530

    Add files via upload

---
+++ diamond pattern .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h> // For abs() function
+
+int main() {
+    int rows = 5;
+    int i, j, space;
+
+    for (i = 1; i <= rows; ++i) {
+        for (space = 1; space <= rows - i; ++space)
+            printf(" ");
+
+        for (j = 1; j <= 2 * i - 1; ++j)
+            printf("*");
+
+        printf("\n");
+    }
+
+    for (i = rows - 1; i >= 1; --i) {
+        for (space = 1; space <= rows - i; ++space)
+            printf(" ");
+
+        for (j = 1; j <= 2 * i - 1; ++j)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
