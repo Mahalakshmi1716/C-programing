8689096c7ae3d28ce0e9d455240807ffb257d0ad
Mahalakshmi1716 <161062244+Mahalakshmi1716@users.noreply.github.com>
2024-03-24 15:12:35 +0530

    Add files via upload

---
+++ bank balance.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int bankbalance=200,withdraw=500;
+	if(500<=200)
+	{
+		printf("you can get money");
+	}
+	else
+	{
+		printf("invalid balance");
+	}
+	return 0;
+}
